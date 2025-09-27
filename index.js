2768e0ad5d73a1157bd2a395f834035e3b60f80e
Abdifatah-Mohamed <yuusufmoharaage@gmail.com>
2025-09-27 20:42:15 +0300

    commited

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hell from Node API');
+});
