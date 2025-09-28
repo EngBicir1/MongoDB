12f33ad897ba28f1140ce9c1e9f91a64af8240d7
Abdifatah-Mohamed <yuusufmoharaage@gmail.com>
2025-09-28 12:14:50 +0300

    comit

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const { MongoClient } = require('mongodb');
-const uri = "mongodb://127.0.0.1:27017";
+const mongoose = require('mongoose');
 
-const client = new MongoClient(uri);
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+    .then(() => {
+        console.log('Database is connected');
+    })
+    .catch((err) => {
+        console.error('Database connection error:', err);
+});
 
-async function connectDB() {
-    try {
-        await client.connect();
-        console.log("Database is connected");
-    } catch (err) {
-        console.error("Database connection error:", err);
-    }
-}
+// schema means is shape of document
+// document is the smallest unit of data in MongoDB.
+// Collection is a group of documents.
+// Database is a group of collections.
 
-connectDB();
+/*
+    const user = [
+        {name : 'user1', age : 20 },
+        {name : 'user2', age : 30 },
+    ]
+
+*/
+
+
+const UserSchema = new mongoose.Schema({
+    name : String,
+    age : Number,
+    isMarried: Boolean,
+    salary: Number,
+    gender: String,
+})
+
+const User = mongoose.model('User',UserSchema);
+
+
